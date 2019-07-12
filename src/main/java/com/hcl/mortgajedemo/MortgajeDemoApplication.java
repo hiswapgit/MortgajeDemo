57ad31c113b7b501a2164fba01564af2c47fda27
hiswapgit <hiswap21@gmail.com>
2019-07-12 10:29:20 +0530

    initial commit

---
+++ src/main/java/com/hcl/mortgajedemo/MortgajeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.mortgajedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgajeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgajeDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hcl/mortgajedemo/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.hcl.mortgajedemo.controller;
